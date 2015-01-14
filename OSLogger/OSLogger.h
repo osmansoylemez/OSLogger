eeceda7d13ad99fffd625f3e58733b655ede0cd3
Osman SÖYLEMEZ <osman.soylemez@veripark.com>
2015-01-14 21:00:25 +0200

    Initial Commit

---
+++ OSLogger/OSLogger.h
@@ -0,0 +1,13 @@
+//
+//  OSLogger.h
+//  OSLogger
+//
+//  Created by Osman Söylemez on 14.01.2015.
+//  Copyright (c) 2015 Osman Söylemez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OSLogger : NSObject
+
+@end
---
+++ OSLogger/OSLogger.m
@@ -0,0 +1,13 @@
+//
+//  OSLogger.m
+//  OSLogger
+//
+//  Created by Osman Söylemez on 14.01.2015.
+//  Copyright (c) 2015 Osman Söylemez. All rights reserved.
+//
+
+#import "OSLogger.h"
+
+@implementation OSLogger
+
+@end
